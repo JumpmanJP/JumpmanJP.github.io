2c2e031307d9a55878d8c13568eca050f658727b
JP O'Grady <jpogrady@JPs-MacBook-Pro.attlocal.net>
2019-11-13 13:42:10 -0500

    Updates

---
+++ precache-manifest.489ca2decb174a0dd6389ea3ff0c5e21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b8ee663554afe2c7ac45c565dc5b250",
+    "url": "/jp_portfolio/index.html"
+  },
+  {
+    "revision": "7b8f74bfb8a5ab349395",
+    "url": "/jp_portfolio/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "5b024c2e96a4cc0d8305",
+    "url": "/jp_portfolio/static/css/main.dc17c8cc.chunk.css"
+  },
+  {
+    "revision": "7b8f74bfb8a5ab349395",
+    "url": "/jp_portfolio/static/js/2.e95ce8f8.chunk.js"
+  },
+  {
+    "revision": "5b024c2e96a4cc0d8305",
+    "url": "/jp_portfolio/static/js/main.83dd6991.chunk.js"
+  },
+  {
+    "revision": "857992d1b3132a1d87b5",
+    "url": "/jp_portfolio/static/js/runtime-main.d9dece1e.js"
+  }
+]);
